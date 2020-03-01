6932a391dba260744dc261b4e012eefa535100dd
Felipe-Santana <felipesantanaalbuquerque9@gmail.com>
2020-03-01 17:59:44 -0300

    Add spring-boot application

---
+++ saferoute-api/src/main/java/br/com/saferoute/SafeRouteApplication.java
@@ -0,0 +1,13 @@
+package br.com.saferoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeRouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeRouteApplication.class, args);
+    }
+
+}
---
+++ saferoute-api/src/test/java/br/com/saferoute/SafeRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.saferoute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeRouteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
